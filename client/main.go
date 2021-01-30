7a4297ed2226a425d73fb63df53dd64ff3b620d8
Pavel Shvedko <pshvedko@gmail.com>
2021-01-30 06:58:02 +0300

    add error codes

--- client/main.go
+++ client/main.go
@@ -16,14 +16,27 @@ func main() {
 	}
 	defer dial.Close()
 	c := product.NewProductServiceClient(dial)
-	var reply *product.FetchReply
-	reply, err = c.Fetch(context.TODO(), &product.FetchQuery{Url: "http://example.com/"})
+	var reply *product.ListReply
+	reply, err = c.List(context.TODO(), &product.ListQuery{Page: &product.Page{
+		Limit:  10,
+		Offset: 20,
+	}, Sort: []*product.Sort{{
+		Order: false,
+		By:    "price",
+	}, {
+		Order: true,
+		By:    "name",
+	}}})
 	if err != nil {
 		log.Fatal(err)
 	}
 	encoder := json.NewEncoder(os.Stdout)
 	encoder.SetIndent("", "  ")
 	err = encoder.Encode(reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = c.Fetch(context.TODO(), &product.FetchQuery{Url: "http://example.com/"})
 	if err != nil {
 		log.Fatal(err)
 	}
--- product/product.go
+++ product/product.go
@@ -4,18 +4,33 @@ package product
 
 import (
 	"context"
-	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 )
 
 type Server struct{}
 
 func (s Server) Fetch(_ context.Context, query *FetchQuery) (*FetchReply, error) {
-	log.Print(query.Url)
-	return &FetchReply{Size: 3}, nil
+	log.Printf("%v", query)
+	return nil, status.Error(codes.Unimplemented, "not implemented yet")
 }
 
 func (s Server) List(_ context.Context, query *ListQuery) (*ListReply, error) {
-	log.Print(query.Page, query.Sort)
-	return nil, errors.New("not found")
+	log.Printf("%v", query)
+	return &ListReply{Products: []*Product{
+		{
+			Name:    "1",
+			Price:   "1",
+			Changes: 10,
+			Date:    timestamppb.Now(),
+		},
+		{
+			Name:    "2",
+			Price:   "2",
+			Changes: 20,
+			Date:    timestamppb.Now(),
+		},
+	}}, nil
 }
