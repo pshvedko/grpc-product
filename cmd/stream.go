8c39f680b941ed7f48bb9651c1334756688a7f53
Pavel Shvedko <pshvedko@gmail.com>
2021-02-03 23:23:38 +0300

    add key press

--- cmd/stream.go
+++ cmd/stream.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"github.com/eiannone/keyboard"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var streamCmd = &cobra.Command{
@@ -11,8 +13,23 @@ var streamCmd = &cobra.Command{
 	RunE:  runStream,
 }
 
-func runStream(*cobra.Command, []string) error {
-	return nil
+func runStream(*cobra.Command, []string) (err error) {
+	log.Println("Press any key...")
+	var key keyboard.Key
+	for {
+		_, key, err = keyboard.GetSingleKey()
+		if err != nil {
+			return
+		}
+		switch key {
+		case keyboard.KeyEsc:
+			return
+		case keyboard.KeyEnter:
+			log.Println("ENTER key pressed!")
+		case keyboard.KeySpace:
+			log.Println("SPACE key pressed!")
+		}
+	}
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pshvedko/grpc-product
 go 1.13
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/golang/mock v1.3.1
 	github.com/golang/protobuf v1.4.3
